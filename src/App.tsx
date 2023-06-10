8ad804a67b8dbb0c2cbdd373c445e047545c5242
Kevin Uehara <kevin.uehara@ifood.com.br>
2023-06-09 21:59:14 -0300

    feat: Adding Sentry integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { initializeSentry } from "./sentry";
+
+initializeSentry();
 
 function App() {
-  const [count, setCount] = useState(0)
+  const handleError = () => {
+    throw new Error("Hi! I'm an error");
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button className="button-error" onClick={handleError}>
+        Click to Explode
+      </button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
