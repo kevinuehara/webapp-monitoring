b62562c29c838466fd449d112f5d955191463c9e
Kevin Uehara <kevin.uehara@ifood.com.br>
2023-06-24 18:21:33 -0300

    refact: Adding integrations folder

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { initializeSentry } from "./sentry/index.ts";
+import { initializeSentry } from "./integrations/sentry/index.ts";
 
 initializeSentry();
 
