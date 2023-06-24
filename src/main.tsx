097894d7fedccc8ee0d067bf11763bb8c2d50685
Kevin Uehara <kevin.uehara@ifood.com.br>
2023-06-24 18:40:25 -0300

    feat(amplitude): Adding amplitude integration

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { initializeAmplitude } from "./integrations/amplitude/index.tsx";
+
+initializeAmplitude();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,13 @@
 import { Content } from "../components/Content";
+import { CustomButton } from "../components/CustomButton";
 import { Header } from "../components/Header";
+import { trackEvent } from "../integrations/amplitude";
 
 export const Home = () => {
+  const handleEventAmplitude = () => {
+    trackEvent("Clicked on Button to Send Event");
+  };
+
   return (
     <>
       <Header />
@@ -10,6 +16,10 @@ export const Home = () => {
         <h2 className="mb-2 text-gray-700">
           Demo Application to Monitoring WebApps
         </h2>
+        <CustomButton
+          text="Send Amplitude Event"
+          onClick={handleEventAmplitude}
+        />
       </Content>
     </>
   );
